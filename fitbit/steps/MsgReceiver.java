c4c4f37d596e2d3802dd57d7dbcc6c0f1dcb90e0
trentandraka <stealthbomber10@gmail.com>
2016-12-02 03:39:49 -0600

    Add files via upload
    Moving files from school-associated Bitbucket account to personal Github for more ease of use / accesibility

---
+++ fitbit/steps/MsgReceiver.java
@@ -0,0 +1,198 @@
+/*
+ * @author Trent Andraka
+ * @author Austin Stover
+ * Rudimentary Fitbit with SparkFun
+ * Arduino, tracks steps, amount of sleep,
+ * and temperature. On Java/PC side, takes
+ * in data, calculates steps, and displays
+ * data.
+ */
+
+package fitbit.steps;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.*;
+
+public class MsgReceiver
+{
+	final private DataInputStream dataStream;
+	//final private PrintStream ps;
+	//final private ViewInputStream psp = new ViewInputStream(ps);
+
+	static final private int MAGIC_NUM = '!';
+
+	static final private int DEBUG_STRING = 0x30; //UTF encoded String
+	static final private int ERROR_STRING = 0x31; //UTF encoded String
+	static final private int TIMESTAMP = 0x32; //4-Byte Integer
+	static final private int ACCEL_X = 0x33; //4-Byte Float
+	static final private int ACCEL_Y = 0x34; //4-Byte Float
+	static final private int ACCEL_Z = 0x35; //4-Byte Float
+	static final private int NUM_STEPS = 0x36; //2 byte integer
+	static final private int CONVERTED_TEMP = 0x37; //4-Byte Float in degrees C
+	static final private int SLEEP_TIME = 0x38; //4-Byte Integer
+	static final private int RESET_STEPS = 0x39;
+	static final private double ACCEL_THRESHOLD = 1.5; //use if calculating steps with threshold
+
+	private ArrayList<Float> accelx = new ArrayList<Float>();
+	private ArrayList<Float> accely = new ArrayList<Float>();
+	private ArrayList<Float> accelz = new ArrayList<Float>();
+	private ArrayList<Integer> times = new ArrayList<Integer>();
+
+	private String sleepTime;
+	private String convTemp;
+	private String timeStamp = "1";
+	private String itemz;
+	private String numSteps = "0";
+
+	public MsgReceiver(InputStream in) throws IOException
+	{
+		dataStream = new DataInputStream(new assign8.steps.ViewInputStream(in));
+	}
+
+	public void run() throws IOException, InterruptedException
+	{
+		while(true)
+		{
+			if(dataStream.available() > 0)
+			{
+				if(dataStream.readByte() == MAGIC_NUM)
+				{
+					byte type = dataStream.readByte();
+					String inputData = readData(type);
+					System.out.println(inputData);
+				}
+			}
+		}
+	}
+
+	String readData(byte type) throws IOException
+	{
+		switch(type)
+		{
+			case DEBUG_STRING:
+				System.out.print("Debug: ");
+				return dataStream.readUTF();
+
+			case ERROR_STRING:
+				System.out.print("Error: ");
+				return "!!!!! " + dataStream.readUTF();
+
+			case TIMESTAMP:
+				System.out.print("Timestamp: ");
+				timeStamp = Integer.toString(dataStream.readInt());
+				return timeStamp;
+
+			case ACCEL_X:
+				System.out.print("X-Acceleration: ");
+				String itemx = Float.toString(readNextFourBytes());
+				accelx.add(Float.parseFloat(itemx));
+				return itemx;
+
+			case ACCEL_Y:
+				System.out.print("Y-Acceleration: ");
+				String itemy = Float.toString(readNextFourBytes());
+				accely.add(Float.parseFloat(itemy));
+				return itemy;
+
+			case ACCEL_Z:
+				System.out.print("Z-Acceleration: ");
+				itemz = Float.toString(readNextFourBytes());
+				accelz.add(Float.parseFloat(itemz));
+				return itemz;
+
+			case NUM_STEPS:
+				System.out.print("Step Count: ");
+				numSteps = Integer.toString(calcSteps());
+				return numSteps;
+
+			case CONVERTED_TEMP:
+				System.out.print("Converted Temp: ");
+				convTemp = Float.toString(readNextFourBytes());        //dataStream.readFloat());
+
+				return convTemp;
+
+			case SLEEP_TIME:
+				System.out.print("Sleep Time: ");
+				sleepTime = Integer.toString(dataStream.readInt());        //dataStream.readFloat());
+
+
+				return sleepTime;
+
+			case RESET_STEPS:
+				reset();
+				return "STEP COUNT RESET";
+
+			default:
+				return null;
+		}
+	}
+
+	private float readNextFourBytes() throws IOException
+	{
+		byte[] fourByte = new byte[4];
+		for(int i = 0; i < 4; i++)
+		{
+			fourByte[i] = dataStream.readByte();
+		}
+		float num = ByteBuffer.wrap(fourByte).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+		return num;
+	}
+
+	public void drawAccel() {
+
+	}
+
+	public int calcSteps() {
+		int size = accelz.size();
+		int steps = 0;
+		//UNCOMMENT IF USING CALCULATED MEAN TO MEASURE PEAKS (IS MORE VOLATILE)
+//		float mean = 0;
+//		float sum = 0;
+//		if (size < 3) {
+//			return 0;
+//		}
+//		for (int i = 0; i < size; i++) {
+//			sum += accelz.get(i);
+//		}
+//		mean = sum / size;
+		for (int i = 1; i < size - 1; i++) {
+			if ((accelz.get(i) > accelz.get(i - 1))
+					&& (accelz.get(i) > accelz.get(i + 1))
+					&& (accelz.get(i) > ACCEL_THRESHOLD)) {
+				steps += 1;
+			}
+		}
+		return steps;
+
+	}
+
+	public void reset() {
+		for (int k = 0; k < accelz.size(); k++) {
+			accelz.remove(k);
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+        try
+        {
+            SerialComm s = new SerialComm();
+            s.connect("COM3"); // Adjust this to be the right port
+            InputStream in = s.getInputStream();
+            MsgReceiver msgr = new MsgReceiver(in);
+            msgr.run();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+	}
+
+}
---
+++ fitbit/steps/steps.ino
@@ -0,0 +1,218 @@
+/*
+ * @author Trent Andraka
+ * @author Austin Stover
+ * Rudimentary Fitbit with SparkFun
+ * Arduino, tracks steps, amount of sleep,
+ * and temperature. On Arduino side,
+ * gathers and takes in data. Also
+ * determines whether subject is
+ * in sleep mode based on given threshold,
+ * whih can be adjusted
+ */
+
+#include <Wire.h>
+#include <SparkFun_MMA8452Q.h>
+
+const int SLEEP_PIN = A0;
+const int PEDO_PIN = A1;
+const int THERMO_PIN = A2;
+const int LED_PIN = 13;
+
+const int MAGIC_NUM = '!';
+const int DEBUG_STRING = 0x30;
+const int ERROR_STRING = 0x31;
+const int TIMESTAMP = 0x32;
+const int ACCEL_X = 0x33;
+const int ACCEL_Y = 0x34;
+const int ACCEL_Z = 0x35;
+const int NUM_STEPS = 0x36;
+const int CONVERTED_TEMP = 0x37;
+const int SLEEP_TIME = 0x38;
+const int RESET_STEPS = 0x39;
+
+const int SLEEP_THRESHOLD = 2.0;  //Derived from testing of acceleration values
+                                  //Represents magnitude, simulates slight tossing/turning
+long count = 0;
+bool ledOn = false;
+unsigned long amountOfSleep = 0;
+unsigned long lastTime = 0;
+
+float accelx;
+float accely;
+float accelz;
+
+MMA8452Q accel;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(DEFAULT);
+  pinMode(SLEEP_PIN, INPUT_PULLUP);
+  pinMode(PEDO_PIN, INPUT_PULLUP);
+  pinMode(THERMO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH); //pedometer mode by default
+  accel.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (accel.available()) {
+    
+    accel.read();
+    accelx = accel.cx;
+    accely = accel.cy;
+    accelz = accel.cz;
+    float currentConvertedTemp = convertedTemp();
+    
+    if (accelMag() > 10.0) {
+      sendError("YOU ARE OUT OF CONTROL");
+    }
+    else {
+      //sendAccelData(accelx, 'x');
+      //sendAccelData(accely, 'y');
+      sendAccelData(accelz, 'z');
+      sendTimestamp(millis());
+      sendSteps();
+    }
+    while (!digitalRead(PEDO_PIN)) {
+      sendReset();
+    }
+    //amountOfSleep = 0;
+    sendConvertedTemp(currentConvertedTemp);
+    if (!digitalRead(SLEEP_PIN)) {
+      unsigned long startTime = millis();
+      while (digitalRead(PEDO_PIN)) {
+        
+        digitalWrite(LED_PIN, LOW);
+        accel.read();
+        accelx = accel.cx;
+        accely = accel.cy;
+        accelz = accel.cz;
+        //sendAccelData(accelx, 'x');
+        //sendAccelData(accely, 'y');
+        sendAccelData(accelz, 'z');
+        sendTimestamp(millis());
+        while (accelMag() > SLEEP_THRESHOLD) {
+          accel.read();
+          accelx = accel.cx;
+          accely = accel.cy;
+          accelz = accel.cz;
+          //sendAccelData(accelx, 'x');
+          //sendAccelData(accely, 'y');
+          sendAccelData(accelz, 'z');
+          sendTimestamp(millis());
+          sendError("YOU ARE NOT ASLEEP");
+          startTime = millis();
+        }
+        amountOfSleep += millis() - startTime;
+        startTime = millis();
+        sendSleepTime(amountOfSleep);
+        }
+      
+      //sendSleepTime(amountOfSleep);
+    }
+  }
+}
+
+float convertedTemp() {
+  float voltage = (5.0/1023) * analogRead(THERMO_PIN);
+  float temp = 25 + (voltage - 0.75) * 100;
+  return temp;
+}
+
+void sendDebug(String message) {
+  int len = message.length();
+  Serial.write(MAGIC_NUM);
+  Serial.write(DEBUG_STRING);
+  Serial.write(len >> 8);
+  Serial.write(len);
+  for (int n = 0; n < len; n++) {
+    Serial.write(message.charAt(n));
+  }
+}
+
+void sendError(String message) {
+  int len = message.length();
+  Serial.write(MAGIC_NUM);
+  Serial.write(ERROR_STRING);
+  Serial.write(len >> 8);
+  Serial.write(len);
+  for (int n = 0; n < len; n++) {
+    Serial.write(message.charAt(n));
+  }
+}
+
+void sendSteps() {
+  Serial.write(MAGIC_NUM);
+  Serial.write(NUM_STEPS);
+}
+
+void sendTimestamp(unsigned long timestamp) {
+  Serial.write(MAGIC_NUM);
+  Serial.write(TIMESTAMP);
+  Serial.write(timestamp >> 24);
+  Serial.write(timestamp >> 16);
+  Serial.write(timestamp >> 8);
+  Serial.write(timestamp);
+}
+
+void sendSleepTime(unsigned long timestamp) {
+  Serial.write(MAGIC_NUM);
+  Serial.write(SLEEP_TIME);
+  Serial.write(timestamp >> 24);
+  Serial.write(timestamp >> 16);
+  Serial.write(timestamp >> 8);
+  Serial.write(timestamp);
+}
+
+void sendConvertedTemp(float celsius) {
+  Serial.write(MAGIC_NUM);
+  Serial.write(CONVERTED_TEMP);
+
+  union data {
+    float a;
+    byte datas[4];
+  }
+  data;
+
+  data.a = celsius;
+  for (int i = 0; i < 4; i++) {
+    Serial.write(data.datas[i]);
+  }
+}
+
+void sendAccelData(float accel, char c) {
+  Serial.write(MAGIC_NUM);
+  if (c == 'x') {
+    Serial.write(ACCEL_X);
+  }
+  else if (c == 'y') {
+    Serial.write(ACCEL_Y);
+  }
+  else {
+    Serial.write(ACCEL_Z);
+  }
+
+  union data {
+    float a;
+    byte datas[4];
+  }
+  data;
+
+  data.a = accel;
+  for (int i = 0; i < 4; i++) {
+    Serial.write(data.datas[i]);
+  }
+}
+
+float accelMag() {
+  return sqrt(sq(accelx) + sq(accely) + sq(accelz));
+}
+
+void sendReset() {
+  Serial.write(MAGIC_NUM);
+  Serial.write(RESET_STEPS);
+}
+
